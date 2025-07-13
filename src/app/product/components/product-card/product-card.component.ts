21f4b56e183d2cf0e92f91250835722cbff284b4
Milton Munoz <livemilton@gmail.com>
2025-07-12 22:04:26 -0500

    teslo-shop :sparkles: show the products in products card

--- src/app/product/components/product-card/product-card.component.ts
+++ src/app/product/components/product-card/product-card.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Product } from '../../interfaces/product.interface';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, SlicePipe],
   templateUrl: './product-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardComponent { }
+export class ProductCardComponent { 
+
+  product = input.required<Product>();
+
+  imageUrl = computed(()=>{
+    return `http://localhost:3000/api/files/product/${this.product().images[0]}`;
+  })
+
+}
