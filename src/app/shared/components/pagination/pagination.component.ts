6c2e3a44c5bb818b4215c73f85c932477d2db77a
Milton Munoz <livemilton@gmail.com>
2025-07-15 20:26:00 -0500

    teslo-shop :sparkles: local pagination functionality

--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, linkedSignal, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,6 +12,8 @@ export class PaginationComponent {
   pages = input(0);
 
   currentPage = input<number>(1);
+
+  activePage = linkedSignal(this.currentPage);
 
   getPagesList = computed(()=>{
     return Array.from({ length: this.pages()}, (_,i) => i +1);
