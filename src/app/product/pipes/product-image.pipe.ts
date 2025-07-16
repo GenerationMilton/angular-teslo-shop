4ba0134dda8cce3ca98f4c869ca72ab9fd6a993a
Milton Munoz <livemilton@gmail.com>
2025-07-15 21:19:23 -0500

    teslo-shop :sparkles: fix pipe to show image of one product

--- src/app/product/pipes/product-image.pipe.ts
+++ src/app/product/pipes/product-image.pipe.ts
@@ -4,22 +4,20 @@ import { environment } from 'src/environments/environment.development';
 const baseUrl = environment.baseUrl;
 
 @Pipe({
-    name: 'productImage'
+  name: 'productImage',
 })
-
 export class ProductImagePipe implements PipeTransform {
-    transform(value: string | string [] ): string {
-        
-        if(typeof value === 'string'){
-            return  `${ baseUrl }/files/products/${ value }`
-        }
-        const image = value.at(0);
-
-        if( !image ){
-            return './assets/images/no-image.jpg'
-        }
-
-        return  `${baseUrl}/files/product/${image}`;
-
+  transform(value: string | string[]): string {
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
     }
+
+    const image = value.at(0);
+
+    if (!image) {
+      return './assets/images/no-image.jpg';
+    }
+
+    return `${baseUrl}/files/product/${image}`;
+  }
 }
