0b0b4a60c957dfb37ae05d0649fbea172ca1613c
Milton Munoz <livemilton@gmail.com>
2025-07-18 21:11:57 -0500

    teslo-shop :sparkles: reactive form to authenticate

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent { }
+export class LoginPageComponent { 
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit(){
+    if(this.loginForm.invalid){
+      this.hasError.set(true)
+      setTimeout(() => {
+        this.hasError.set(false)
+      }, 2000);
+      return;
+    }
+
+    const { email= '', password=''} = this.loginForm.value;
+
+    console.log({ email, password });
+  
+  }
+}
