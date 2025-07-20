75e79cac71502e67fc09fa578bcba9d4516c22a5
Milton Munoz <livemilton@gmail.com>
2025-07-20 13:50:25 -0500

    teslo-shop :sparkles: administrative panel - product form complete

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,16 +1,54 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 import { Product } from 'src/app/product/interfaces/product.interface';
 import { ProductCarouselComponent } from "src/app/product/product-carousel/product-carousel.component";
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent],
+  imports: [ProductCarouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductDetailsComponent { 
+export class ProductDetailsComponent implements OnInit {
+
+
   product = input.required<Product>();
 
+  //reactive form
+  fb = inject(FormBuilder);
+
+  productForm= this.fb.group({
+    title:['',Validators.required],
+    description: ['', Validators.required],
+    slug: ['',[Validators.required, Validators.pattern(FormUtils.slugPattern)]
+    ],
+    price:[0, [Validators.required, Validators.min(0)]], 
+    stock:[0, [Validators.required, Validators.min(0)]], 
+    sizes:[['']], 
+    images: [[]],
+    tags:[''],
+    gender: [
+      'men',
+      [Validators.required, Validators.pattern(/men|women|kid|unisex/)],
+    ]
+  })
+
   sizes =['XS',"S","M","L","XL","XXL"];
+
+    ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>){
+    this.productForm.reset(this.product()as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',')});
+    //this.productForm.patchValue(formLike as any);
+  }
+
+  onSubmit() {
+
+    console.log(this.productForm.value);
+  } 
 
 }
