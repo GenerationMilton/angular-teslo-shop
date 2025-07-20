bd08f230cfe3e79b87bf714e4dfa83e6aab238b7
Milton Munoz <livemilton@gmail.com>
2025-07-19 20:37:06 -0500

    teslo-shop :sparkles: show products with pagination

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,10 +1,34 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 import { ProductTableComponent } from "src/app/product/product-table/product-table.component";
+import { ProductsService } from 'src/app/product/services/products.service';
+import { PaginationComponent } from "@shared/components/pagination/pagination.component";
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTableComponent, ProductTableComponent],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+
+   productsService = inject(ProductsService);
+   paginationService = inject(PaginationService);
+
+   productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({ 
+      page: this.paginationService.currentPage()-1,
+      limit: this.productsPerPage()
+    }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({
+        offset: params.page * 9,
+        limit:params.limit
+      });
+    },
+  });
+
+ }
