eb3ca48baf81e7ab2b0a7e46eb58cd176b7ab451
Milton Munoz <livemilton@gmail.com>
2025-07-19 20:59:18 -0500

    teslo-shop :sparkles: product page by id

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,9 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from 'src/app/product/services/products.service';
 
 @Component({
   selector: 'app-product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductAdminPageComponent { }
+export class ProductAdminPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router)
+  productService = inject(ProductsService);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(
+      map(params => params['id'])
+    )
+  );
+
+  productResource = rxResource({
+    params: ()=> ({ id: this.productId()}),
+    stream: ({ params }) =>{
+      return this.productService.getProductById(params.id)
+    },
+  });
+
+  redirectEffect = effect(()=>{
+    if(this.productResource.error()){
+      this.router.navigate(['/admin/products'])
+    }
+  })
+
+
+ }
